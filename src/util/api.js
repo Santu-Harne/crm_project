44a71d8bd361f258b0c70a3c27dccb2505ed77c0
Santosh Kumar <santosh.283143@gmail.com>
2023-08-31 14:52:41 +0530

    updated

--- src/util/api.js
+++ src/util/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Create an Axios instance
 // const token = localStorage.getItem('token');
 const api = axios.create({
-  baseURL: 'http://crm-made-simple-f8a83a9caed2.herokuapp.com'
+  baseURL: 'https://crm-made-simple-f8a83a9caed2.herokuapp.com'
 });
 
 // Request interceptor to add the JWT token to the Authorization header
